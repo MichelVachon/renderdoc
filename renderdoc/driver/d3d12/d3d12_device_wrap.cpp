6d1584b36cecd86941ea7912a094319a0b4bb0af
MichelVachon <32905270+MichelVachon@users.noreply.github.com>
2018-12-07 09:05:52 -0500

    Add OpenSharedHandle partial support
    Add OpenSharedHandle partial (write only, no initial state) support very much like an ordinary CreateCommittedResource

--- renderdoc/driver/d3d12/d3d12_device_wrap.cpp
+++ renderdoc/driver/d3d12/d3d12_device_wrap.cpp
@@ -22,6 +22,8 @@
  * THE SOFTWARE.
  ******************************************************************************/
 
+__pragma(optimize("",off))
+
 #include "d3d12_device.h"
 #include "driver/dxgi/dxgi_common.h"
 #include "driver/ihv/amd/official/DXExt/AmdExtD3D.h"
@@ -69,9 +71,6 @@ bool WrappedID3D12Device::Serialise_CreateCommandQueue(SerialiserType &ser,
       if(Descriptor.Type == D3D12_COMMAND_LIST_TYPE_DIRECT && m_Queue == NULL)
       {
         m_Queue = wrapped;
-        // we hold an extra ref on this during capture to keep it alive, for simplicity match that
-        // behaviour here.
-        m_Queue->AddRef();
         CreateInternalResources();
       }
 
@@ -124,9 +123,6 @@ HRESULT WrappedID3D12Device::CreateCommandQueue(const D3D12_COMMAND_QUEUE_DESC *
     if(pDesc->Type == D3D12_COMMAND_LIST_TYPE_DIRECT && m_Queue == NULL)
     {
       m_Queue = wrapped;
-      // keep this queue alive even if the application frees it, for our own use
-      m_Queue->AddRef();
-      InternalRef();
       CreateInternalResources();
     }
 
@@ -2134,8 +2130,87 @@ void WrappedID3D12Device::CopyDescriptorsSimple(UINT NumDescriptors,
 
 HRESULT WrappedID3D12Device::OpenSharedHandle(HANDLE NTHandle, REFIID riid, void **ppvObj)
 {
-  D3D12NOTIMP("Shared Handles / API interop");
-  return m_pDevice->OpenSharedHandle(NTHandle, riid, ppvObj);
+  if(IsReplayMode(m_State))
+  {
+    RDCERR("Don't support opening shared handle during replay.");
+    return E_NOTIMPL;
+  }
+
+  if(ppvObj == NULL)
+    return E_INVALIDARG;
+
+  bool isRes = (riid == __uuidof(ID3D12Resource) ? true : false);
+  if(isRes)
+  {
+    void *res = NULL;
+    HRESULT hr;
+    SERIALISE_TIME_CALL(hr = m_pDevice->OpenSharedHandle(NTHandle, riid, &res));
+
+    if(FAILED(hr))
+    {
+      IUnknown *unk = (IUnknown *)res;
+      SAFE_RELEASE(unk);
+      return hr;
+    }
+    else
+    {
+      ID3D12Resource *real = (ID3D12Resource *)res;
+
+      D3D12_HEAP_PROPERTIES heapProperties;
+      D3D12_HEAP_FLAGS heapFlags;
+      real->GetHeapProperties( &heapProperties, &heapFlags );
+      const D3D12_RESOURCE_DESC desc = real->GetDesc();
+
+      D3D12_RESOURCE_STATES InitialResourceState = D3D12_RESOURCE_STATE_GENERIC_READ;
+      const D3D12_CLEAR_VALUE *pOptimizedClearValue = nullptr;
+
+      WrappedID3D12Resource *wrapped = new WrappedID3D12Resource(real, this);
+
+      if(IsCaptureMode(m_State))
+      {
+        CACHE_THREAD_SERIALISER();
+      
+        SCOPED_SERIALISE_CHUNK(D3D12Chunk::Device_CreateCommittedResource);
+        Serialise_CreateCommittedResource(ser, &heapProperties, heapFlags, &desc, InitialResourceState,
+                                          pOptimizedClearValue, riid, (void **)&wrapped);
+      
+        D3D12ResourceRecord *record = GetResourceManager()->AddResourceRecord(wrapped->GetResourceID());
+        record->type = Resource_Resource;
+        record->Length = 0;
+        wrapped->SetResourceRecord(record);
+      
+        record->m_MapsCount = GetNumSubresources(this, &desc);
+        record->m_Maps = new D3D12ResourceRecord::MapData[record->m_MapsCount];
+      
+        record->AddChunk(scope.Get());
+      
+        {
+          SCOPED_READLOCK(m_CapTransitionLock);
+          if(IsBackgroundCapturing(m_State))
+            GetResourceManager()->MarkDirtyResource(wrapped->GetResourceID());
+          else
+            GetResourceManager()->MarkPendingDirty(wrapped->GetResourceID());
+        }
+      }
+      else
+      {
+        GetResourceManager()->AddLiveResource(wrapped->GetResourceID(), wrapped);
+      }
+      
+      {
+        SCOPED_LOCK(m_ResourceStatesLock);
+        SubresourceStateVector &states = m_ResourceStates[wrapped->GetResourceID()];
+      
+        states.resize(GetNumSubresources(m_pDevice, &desc), InitialResourceState);
+      }
+      
+      *ppvObj = (ID3D12Resource *)wrapped;
+    }
+
+    return S_OK;
+  }
+
+  return E_NOINTERFACE;
 }
 
 HRESULT WrappedID3D12Device::OpenSharedHandleByName(LPCWSTR Name, DWORD Access, HANDLE *pNTHandle)
